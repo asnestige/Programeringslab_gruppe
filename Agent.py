835ddeb12bb8a2f7f34d52a1a91961f7758e8fba
Åsne Stige <asnestige@gmail.com>
2018-10-19 15:44:03 +0200

    Endre agent

--- Agent.py
+++ Agent.py
@@ -3,30 +3,30 @@ from Ledboard import Ledboard as led
 
 class Agent:
     def __init__(self, keypad, led_board, temp_password, pathname, Lid, Ldur):
-        self.keypad = keypad #a pointer to the keypad
-        self.led_board = led_board #pointer to the LED Board
+        self.keypad = keypad  # a pointer to the keypad
+        self.led_board = led_board  # pointer to the LED Board
         self.temp_password = temp_password
-        self.pathname = pathname  #pathname to the file holding the KPC’s password
+        self.pathname = pathname  # pathname to the file holding the KPC’s password
         self.override_signal = None
-        self.led_id = Lid  #slots for holding the LED id
+        self.led_id = Lid  # slots for holding the LED id
         self.led_time = Ldur
 
     def activate_agent(self):
         return
 
-    def null_action(self): #Resetter agenten (?) og passord(?)
+    def null_action(self):  # Resetter agenten (?) og passord(?)
         pass
 
-    def get_next_signal(self):
+    def get_next_signal(self):  # Return the override-signal, if it is non-blank; otherwise query the keypad for the next pressed key.
         if self.override_signal != None:
             return self.override_signal
         self.keypad.get_next_signal()
 
-    def verify_login(self): #lese filen og sjekke om passordet stemmer
+    def verify_login(self):  # lese filen og sjekke om passordet stemmer
         file = open("self.pathname.txt", "r")
-        password = file.read() #leser inn filen og oppretter en streng med ordene
+        password = file.read()  # leser inn filen og oppretter en streng med ordene
         file.close()
-        if password == self.temp_password: #sjekker om passordet lagret i filen er lik passordet tastet inn
+        if password == self.temp_password:  # sjekker om passordet lagret i filen er lik passordet tastet inn
             self.override_signal = 'True'
             return True
         self.override_signal = 'False'
@@ -38,6 +38,7 @@ class Agent:
         return False
 
     def startup(self):  # Få lys til å blinke og reset password
+        self.clear_password()
         self.led_board.startup_leds()
 
     def login(self):  # Twinkle lights og verify login
@@ -55,9 +56,6 @@ class Agent:
     def add_symbol_password(self):
         self.temp_password + "" + str(self.get_next_signal())  # Legg til det vi skriver inn i keypaden
 
-    #def reset_password(self):
-    #    self.cach_password()
-
     def clear_password(self):
         self.temp_password = ""
 
@@ -66,67 +64,25 @@ class Agent:
             f = open("self.pathname.txt", "w")
             f.write(self.temp_password)
             f.close()
+            self.flash_leds()  # If password changed
+        self.twinkle_leds()  # If fail
 
     #LYS
     def set_led_id(self):
-        self.led_id = self.get_next_signal() #Setter id til det vi har trykket på keypaden
+        self.led_id = self.get_next_signal()  # Setter id til det vi har trykket på keypaden
 
     def set_led_time(self):
-        self.led_time + "" + str(self.get_next_signal()) #Legger til taller vi har skrevet inn i ledd helt til vi trykker *
+        self.led_time + "" + str(self.get_next_signal())  # Legger til taller vi har skrevet inn i ledd helt til vi trykker *
 
     def reset_led(self):
         self.led_time = ""
 
     def light_one_led(self):
-        self.led_board.light_led(self.led_id, self.led_time)
+        self.led_board.light_led(int(self.led_id), int(self.led_time))
 
     def flash_leds(self):
-        self.led_board.flash_all_leds(self.led_time)
+        self.led_board.flash_all_leds(int(self.led_time))
 
     def twinkle_leds(self):
-        self.led_board.twinkle_all_leds(self.led_time)
+        self.led_board.twinkle_all_leds(int(self.led_time))
 
-# • light one led - Using values stored in the Lid and Ldur slots, call the LED Board and request that
-    # LED # Lid be turned on for Ldur seconds.
-    # • flash leds - Call the LED Board and request the flashing of all LEDs.
-    # • twinkle leds - Call the LED Board and request the twinkling of all LEDs.
-    # • exit action - Call the LED Board to initiate the ”power down” lighting sequence
-
-#• init passcode entry - Clear the passcode-buffer and initiate a ”power up” lighting sequence on the LED
-#Board. This should be done when the user first presses the keypad.
-#• get next signal - Return the override-signal, if it is non-blank; otherwise query the keypad for the next
-#pressed key.
-#• verify login - Check that the password just entered via the keypad matches that in the password file.
-#Store the result (Y or N) in the override-signal. Also, this should call the LED Board to initiate the
-#appropriate lighting pattern for login success or failure.
-#• validate passcode change - Check that the new password is legal. If so, write the new password in the
-#password file. A legal password should be at least 4 digits long and should contain no symbols other
-#than the digits 0-9. As in verify login, this should use the LED Board to signal success or failure in
-#changing the password.2
-#• light one led - Using values stored in the Lid and Ldur slots, call the LED Board and request that
-#LED # Lid be turned on for Ldur seconds.
-#• flash leds - Call the LED Board and request the flashing of all LEDs.
-#• twinkle leds - Call the LED Board and request the twinkling of all LEDs.
-#• exit action - Call the LED Board to initiate the ”power down” lighting sequence
-
-#• init passcode entry - Clear the passcode-buffer and initiate a ”power up” lighting sequence on the LED
-#Board. This should be done when the user first presses the keypad.
-
-#• get next signal - Return the override-signal, if it is non-blank; otherwise query the keypad for the next
-#pressed key.
-
-#• verify login - Check that the password just entered via the keypad matches that in the password file.
-#Store the result (Y or N) in the override-signal. Also, this should call the LED Board to initiate the
-#appropriate lighting pattern for login success or failure.
-
-#• validate passcode change - Check that the new password is legal. If so, write the new password in the
-#password file. A legal password should be at least 4 digits long and should contain no symbols other
-#than the digits 0-9. As in verify login, this should use the LED Board to signal success or failure in
-#changing the password.2
-
-#• light one led - Using values stored in the Lid and Ldur slots, call the LED Board and request that
-#LED # Lid be turned on for Ldur seconds.
-
-#• flash leds - Call the LED Board and request the flashing of all LEDs.
-#• twinkle leds - Call the LED Board and request the twinkling of all LEDs.
-#• exit action - Call the LED Board to initiate the ”power down” lighting sequence
